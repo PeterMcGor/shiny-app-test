f3db3c3c35b050b45fa786b3622130e6929872f2
petermcgor <pedro.macias.gordaliza@gmail.com>
2024-12-17 20:32:28 +0100

    First commit: initial shiny app

---
+++ app.R
@@ -0,0 +1,31 @@
+library(shiny)
+
+# Define UI
+ui <- fluidPage(
+  titlePanel("My First Shiny App"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("number", 
+                  "Choose a number:", 
+                  min = 1,
+                  max = 50,
+                  value = 25)
+    ),
+    
+    mainPanel(
+      h3("You selected:"),
+      textOutput("selected_number")
+    )
+  )
+)
+
+# Define server logic
+server <- function(input, output) {
+  output$selected_number <- renderText({
+    paste("You chose the number", input$number)
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
